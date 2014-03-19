4dbc2a2a75b659c8792c0a4f9c45e65cfc1fb7aa
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2014-03-19 22:45:19 +0100

    Update editor

--- includes/extra-metabox/fields/Editor.php
+++ includes/extra-metabox/fields/Editor.php
@@ -15,17 +15,22 @@ class Editor extends Field {
 
 	public function the_admin($bloc_classes) {
 		?>
-		<div class="bloc <?php echo $bloc_classes; ?>">
+		<div <?php echo (!empty($bloc_classes)) ? ' class="'.$bloc_classes.'"' : ''; ?>>
+			<?php if ($this->title != null) : ?>
+				<h2><?php echo $this->title; ?></h2>
+			<?php endif; ?>
+
 			<?php if ($this->label != null) : ?>
-				<h2><?php echo $this->label; ?></h2>
+				<p class="label"><?php echo $this->label; ?></p>
 			<?php endif; ?>
 
 			<?php $this->mb->the_field($this->get_single_field_name('editor'));
 			$value = apply_filters('the_content', html_entity_decode( $this->mb->get_the_value(), ENT_QUOTES, 'UTF-8' ));
 			wp_editor($value, $this->mb->get_the_name(), array(
-				"textarea_name" => $this->mb->get_the_name(),
-				"tinymce" => array(
-					"body_class" => $this->mb->get_the_name()
+				'textarea_name' => $this->mb->get_the_name(),
+				'editor_height' => 800,
+				'tinymce' => array(
+					'body_class' => $this->mb->get_the_name(),
 				)
 			)); ?>
 		</div>
