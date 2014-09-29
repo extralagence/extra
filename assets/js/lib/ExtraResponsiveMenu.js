87a610c4dcb3d96f6ffe34a275b7c3014b5fbd70
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2014-09-29 10:40:45 +0200

    Responsive menu now listen to resize

--- assets/js/lib/ExtraResponsiveMenu.js
+++ assets/js/lib/ExtraResponsiveMenu.js
@@ -127,6 +127,16 @@ function ExtraResponsiveMenu(options) {
 		}
 	});
 
+	$(document).on('extra.responsive-resize', function(){
+		if (!small && !opt.everySizes) {
+			opt.$menu.removeAttr("style");
+			opt.$site.removeAttr("style");
+			opt.$menu.hide();
+		} else {
+			opt.$menu.show();
+		}
+	});
+
 	// INIT
 	opt.$menu.css('visibility', 'visible');
 	hideMenu(true);
