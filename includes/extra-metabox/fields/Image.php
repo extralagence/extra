2ad4f626d1e891a88f0840db098f1c060ac374a6
Vincent Saïsset <vincent.saisset@gmail.com>
2014-09-30 16:56:27 +0200

    debug field Image

--- includes/extra-metabox/fields/Image.php
+++ includes/extra-metabox/fields/Image.php
@@ -26,9 +26,11 @@ class Image extends AbstractField {
 	public static function init () {
 
 		parent::init();
-        //wp_enqueue_media();
-        wp_enqueue_style('extra-image-gallery-metabox', EXTRA_INCLUDES_URI . '/extra-metabox/css/extra-image-gallery.less');
-        wp_enqueue_script('extra-image-metabox', EXTRA_INCLUDES_URI . '/extra-metabox/js/extra-image.js', array('jquery'), null, true);
+		add_action( 'admin_enqueue_scripts', function () {
+			wp_enqueue_media();
+			wp_enqueue_style('extra-image-gallery-metabox', EXTRA_INCLUDES_URI . '/extra-metabox/css/extra-image-gallery.less');
+			wp_enqueue_script('extra-image-metabox', EXTRA_INCLUDES_URI . '/extra-metabox/js/extra-image.js', array('jquery'), null, true);
+		});
 	}
 
 	public function the_admin() {
