f6ae55feb916f02299ab402bbf7e43f59b4d2358
Vincent Saïsset <vincent.saisset@gmail.com>
2014-10-10 16:51:49 +0200

    Resize ExtraBottomFade

--- assets/js/lib/ExtraBottomFade.js
+++ assets/js/lib/ExtraBottomFade.js
@@ -1,16 +1,11 @@
-function ExtraBottomFade(options) {
+function ExtraBottomFade (options) {
 	var self = this;
-	self.options = $.extend({
-			'$elements' : $(".bottom-fade"),
-			'ease'      : Linear.easeIn
-		}, options);
 
-
-	self.$window = $(window);
-
-	self.locked = false;
 	self.position = function () {
 		var scrollTop = self.$window.scrollTop();
+
+		console.log('scrollTop');
+		console.log(scrollTop);
 
 		self.options.$elements.each(function() {
 			var coords = $(this).data('coords'),
@@ -21,26 +16,43 @@ function ExtraBottomFade(options) {
 		});
 	};
 
-	self.init = function () {
+	self.init = function (options) {
+		self.$window = $(window);
 		self.windowHeight = self.$window.outerHeight();
 
-		var fromTop = parseInt(self.windowHeight/6);
+		self.options = $.extend({
+			'$elements' : $(".bottom-fade"),
+			'ease'      : Linear.easeIn,
+			'fromTop'   : self.windowHeight/6,
+			'min'       : self.windowHeight,
+			'max'       : self.windowHeight * ( 2 / 3)
+		}, options);
 
-		console.log(fromTop);
-
+		var fromTop = parseInt(self.options.fromTop);
 		options.$elements.each(function () {
-			var $this = $(this);
-			$this.data('coords', {
-				min: $this.offset().top - self.windowHeight,
-				max: $this.offset().top - 2 * (self.windowHeight / 3)
-			});
+			var $this = $(this),
+				coords = $this.data('coords');
 
+			if (coords === undefined) {
+				coords = {top:  $this.offset().top};
+			}
+			coords.min = coords.top - self.options.min;
+			coords.max = coords.top - self.options.max;
+			$this.data('coords', coords);
+
+			if ($this.data('tween') !== undefined) {
+				var previousTween = $this.data('tween');
+				previousTween.progress(0);
+				previousTween.kill();
+				TweenMax.set($this, {autoAlpha: 1, top: 0});
+			}
 			var tween = TweenMax.from($this, 1, {autoAlpha: 0, top: fromTop, ease: self.options.ease}).pause();
 			$this.data('tween', tween);
 		});
+
+		self.$window.scroll(self.position);
+		self.position();
 	};
 
-	self.init();
-	self.$window.scroll(self.position);
-	self.position();
+	self.init(options);
 }
