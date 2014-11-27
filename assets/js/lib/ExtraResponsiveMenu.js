f8c46f59a58e0e4404aa2779ac3d99b195f91b9b
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2014-11-27 19:52:19 +0100

    Responsive menu performance improvement. Now with a hookable timeline.

--- assets/js/lib/ExtraResponsiveMenu.js
+++ assets/js/lib/ExtraResponsiveMenu.js
@@ -12,23 +12,27 @@
 function ExtraResponsiveMenu(options) {
 
 	var opt = $.extend({
-			'$menu' : $("#mobile-menu-container"),
-			'$site' : $("#wrapper"),
-			'$button': $("#switch-mobile-menu"),
-			'everySizes' : false,
-			'moveButton' : true
+			$menu : $("#mobile-menu-container"),
+			$site : $("#wrapper"),
+			$button: $("#switch-mobile-menu"),
+			everySizes : false,
+			moveButton : true,
+			prependTimeline: null,
+			appendTimeline: null
 		}, options),
 		menuOpen = true,
 		$html = $('html'),
 		transform3d = $html.hasClass('csstransforms3d'),
 		$wpadminbar = $("#wpadminbar"),
+		timeline = new TimelineMax({paused: true, onReverseComplete: function() {
+			$window.trigger('extra.hideResponsiveMenuComplete');
+		}}),
 		$toMove;
 	if(small) {
 		$toMove = opt.moveButton ? opt.$button : [];
 	} else {
 		$toMove = opt.moveButton ? [opt.$site, opt.$button] : opt.$site;
 	}
-
 	/**************************
 	 *
 	 *
@@ -68,10 +72,42 @@ function ExtraResponsiveMenu(options) {
 		if (menuOpen) {
 			showMenu(true);
 		} else {
-			console.log("coucou hide menu fast");
 			hideMenu(true);
 		}
 	});
+	/**************************
+	 *
+	 *
+	 * CREATE TIMELINE
+	 *
+	 *
+	 *************************/
+	if(opt.prependTimeline) {
+		timeline = opt.prependTimeline(timeline);
+	}
+	if (transform3d) {
+		TweenMax.set(opt.$menu, {x: -opt.$menu.width() + 'px', force3D: true, ease: Strong.EaseIn, onComplete: function() {
+			$window.trigger('extra.hideResponsiveMenuComplete');
+		}});
+		TweenMax.set($toMove, {x: 0, force3D: true, clearProps:"all", ease: Strong.EaseIn});
+	} else {
+		TweenMax.set(opt.$menu, {x: -opt.$menu.width() + 'px', ease: Strong.EaseIn});
+		TweenMax.set($toMove, {x: 0, clearProps:"all", ease: Strong.EaseIn});
+	}
+	if (transform3d) {
+		timeline.to(opt.$menu, 0.4, {x: 0, force3D: true, ease: Quad.EaseOut});
+		timeline.to($toMove, 0.5, {x: opt.$menu.width() + 'px', force3D: true, ease: Quad.EaseOut, onComplete: function() {
+			$window.trigger('extra.showResponsiveMenuComplete');
+		}}, '-=0.3');
+	} else {
+		timeline.to(opt.$menu, 0.4, {x: 0, ease: Quad.EaseOut});
+		timeline.to($toMove, 0.5, {x: opt.$menu.width() + 'px', ease: Quad.EaseOut, onComplete: function() {
+			$window.trigger('extra.showResponsiveMenuComplete');
+		}}, '-=0.3');
+	}
+	if(opt.appendTimeline) {
+		timeline = opt.appendTimeline(timeline);
+	}
 	/**************************
 	 *
 	 *
@@ -84,18 +120,12 @@ function ExtraResponsiveMenu(options) {
 			opt.$menu.css('visibility', 'visible');
 			menuOpen = true;
 			$html.addClass('menu-open');
-			if (transform3d) {
-				TweenMax.to(opt.$menu, (fast === true ? 0 : 0.4), {x: 0, force3D: true, ease: Quad.EaseOut});
-				TweenMax.to($toMove, (fast === true ? 0 : 0.5), {x: opt.$menu.width() + 'px', force3D: true, ease: Quad.EaseOut, onComplete: function() {
-					$window.trigger('extra.showResponsiveMenuComplete');
-				}});
-			} else {
-				TweenMax.to(opt.$menu, (fast === true ? 0 : 0.4), {x: 0, ease: Quad.EaseOut});
-				TweenMax.to($toMove, (fast === true ? 0 : 0.5), {x: opt.$menu.width() + 'px', ease: Quad.EaseOut, onComplete: function() {
-					$window.trigger('extra.showResponsiveMenuComplete');
-				}});
-			}
 			$window.trigger('extra.showResponsiveMenu');
+			if(fast) {
+				timeline.totalProgress(1);
+			} else {
+				timeline.play();
+			}
 		} else {
 			hideMenu(true);
 		}
@@ -110,23 +140,17 @@ function ExtraResponsiveMenu(options) {
 	function hideMenu(fast) {
 		menuOpen = false;
 		$html.removeClass('menu-open');
-		if (transform3d) {
-			TweenMax.to(opt.$menu, (fast === true ? 0 : 0.6), {x: -opt.$menu.width() + 'px', force3D: true, ease: Strong.EaseIn, onComplete: function() {
-				$window.trigger('extra.hideResponsiveMenuComplete');
-			}});
-			TweenMax.to($toMove, (fast === true ? 0 : 0.5), {x: 0, force3D: true, clearProps:"all", ease: Strong.EaseIn});
-		} else {
-			TweenMax.to(opt.$menu, (fast === true ? 0 : 0.6), {x: -opt.$menu.width() + 'px', ease: Strong.EaseIn, onComplete: function() {
-				$window.trigger('extra.hideResponsiveMenuComplete');
-			}});
-			TweenMax.to($toMove, (fast === true ? 0 : 0.5), {x: 0, clearProps:"all", ease: Strong.EaseIn});
-		}
 		if (!small && !opt.everySizes) {
 			opt.$menu.removeAttr("style");
 			opt.$site.removeAttr("style");
 			opt.$menu.hide();
 		}
 		$window.trigger('extra.hideResponsiveMenu');
+		if(fast) {
+			timeline.totalProgress(0);
+		} else {
+			timeline.reverse();
+		}
 	}
 
 	$(document).on('click', function(e) {
