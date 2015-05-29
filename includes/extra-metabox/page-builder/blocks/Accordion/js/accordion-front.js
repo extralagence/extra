81b8e2713ca9e263573558af6a06f1fb10c9f346
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2015-05-29 14:29:33 +0200

    Updated accordion.js

--- includes/extra-metabox/page-builder/blocks/Accordion/js/accordion-front.js
+++ includes/extra-metabox/page-builder/blocks/Accordion/js/accordion-front.js
@@ -5,10 +5,11 @@ $(document).ready(function(){
 	 *
 	 *********************/
 	$(".accordeon-element").each(function(){
-		var wrapper = $(this);
-		var title = wrapper.find(" > .tab-title");
-		var content = wrapper.find(" > .tab-content");
-		var height = content.innerHeight();
+		var wrapper = $(this),
+			title = wrapper.find(" > .tab-title"),
+			content = wrapper.find(" > .tab-content").height(0),
+			inner = content.find(" > .inner"),
+			height = inner.outerHeight();
 		content.height(0);
 		title.bind("click", function(){
 			if(content.height() > 0) {
@@ -16,7 +17,8 @@ $(document).ready(function(){
 				TweenMax.to(content, 0.3, {css:{height:0}});
 			} else {
 				wrapper.addClass("open");
-				TweenMax.to(content, 0.3, {css:{height:height}});
+				height = inner.outerHeight();
+				TweenMax.to(content, 0.5, {css:{height:height}});
 			}
 		});
 	});
