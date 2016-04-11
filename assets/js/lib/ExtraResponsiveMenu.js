85d15a65f5b7a632fea47b8e384f546a16a8607e
Jean-Baptiste Janot <jeanbaptiste.janot@gmail.com>
2016-04-11 10:46:48 +0200

    Updated responsive menu

--- assets/js/lib/ExtraResponsiveMenu.js
+++ assets/js/lib/ExtraResponsiveMenu.js
@@ -1,204 +1,202 @@
-/**************************
+/****************
  *
  *
- * Responsive Navigation
- * @$menu : jquery object
- * @$site : jquery object
- * @$button : jquery object
- * @everySizes : boolean
+ *
+ * EXTRA RESPONSIVE MENU 1.0
+ * Documentation and examples on http://menu.extralagence.com
  *
  *
- *************************/
-function ExtraResponsiveMenu(options) {
+ *
+ ****************/
+(function ($) {
+	'use strict';
+	/*global console, jQuery, $, window, TweenMax, Draggable, Quad */
+	var $window = window.$window || $(window);
 
-	var opt = $.extend({
-			$menu: $("#mobile-menu-container"),
+	$.fn.extraMenu = function (options) {
+
+		var opt = $.extend({
 			$site: $("#wrapper"),
-			$button: $("#switch-mobile-menu"),
-			everySizes: false,
+			$button: $(".extra-menu-button").first(),
+			resizeEvent: 'resize',
+			everySizes: true,
+			smallBreakpointCondition: null,
+			breakPointWidth: 960,
+			autoHide: true,
 			moveButton: true,
+			moveSite: true,
+			hideTriggerEvent: 'extra:menu:hideTrigger',
+			showTriggerEvent: 'extra:menu:showTrigger',
 			prependTimeline: null,
 			appendTimeline: null
-		}, options),
-		menuOpen = true,
-		$html = $('html'),
-		$wpadminbar = $("#wpadminbar"),
-		timeline = new TimelineMax({
-			paused: true,
-			onReverseComplete: function () {
-				$window.trigger('extra.hideResponsiveMenuComplete');
+		}, options);
+
+		this.each(function (index, element) {
+
+			/*********************************** SETUP VARS ***********************************/
+			var $menu = $(this),
+				menuOpen = false,
+				$html = $('html'),
+				smallBreakpoint = false,
+				timeline,
+				$toMove;
+
+			/*********************************** FUNCTIONS ***********************************/
+			// show the menu
+			// @fast : false = hide with animation, true = hide instantaneously
+			function showMenu(fast) {
+				if ((smallBreakpoint && !opt.everySizes) || opt.everySizes) {
+					menuOpen = true;
+					$html.addClass('extra-menu-open');
+					$html.removeClass('extra-menu-close');
+					if (fast) {
+						timeline.totalProgress(1);
+						$window.trigger('extra:menu:ShowComplete');
+					} else {
+						timeline.play();
+					}
+				} else {
+					hideMenu(true);
+				}
 			}
-		}),
-		$toMove;
-	/**************************
-	 *
-	 *
-	 * INIT
-	 *
-	 *
-	 *************************/
-	if (!opt.$menu.length || !opt.$site.length || !opt.$button.length) {
-		console.log("Missing element to initialize the responsive menu");
-		return;
-	}
-	/**************************
-	 *
-	 *
-	 * CLICK
-	 *
-	 *
-	 *************************/
-	opt.$button.click(function () {
-		if (small || opt.everySizes) {
-			if (!menuOpen) {
-				showMenu();
-			} else {
-				hideMenu();
+
+			// hide menu
+			// @fast : false = hide with animation, true = hide instantaneously
+			function hideMenu(fast) {
+				menuOpen = false;
+				$html.removeClass('extra-menu-open');
+				$html.addClass('extra-menu-close');
+				$window.trigger('extra:menu:HideStart');
+				if (fast) {
+					timeline.totalProgress(0);
+					$window.trigger('extra:menu:HideComplete');
+				} else {
+					timeline.reverse();
+				}
 			}
-		}
-		return false;
-	});
-	/**************************
-	 *
-	 *
-	 * ON RESIZE
-	 *
-	 *
-	 *************************/
-	$window.on('extra.resize', function () {
-		if (menuOpen) {
-			showMenu(true);
-		} else {
-			hideMenu(true);
-		}
-	});
-	/**************************
-	 *
-	 *
-	 * INIT
-	 *
-	 *
-	 *************************/
-	opt.$menu.css('visibility', 'visible');
-	update();
-	/**************************
-	 *
-	 *
-	 * CREATE TIMELINE
-	 *
-	 *
-	 *************************/
-	// START OF TIMELINE
-	if (opt.prependTimeline) {
-		timeline = opt.prependTimeline(timeline);
-	}
 
-	// SETTER
-	TweenMax.set(opt.$menu, {
-		x: -opt.$menu.width() + 'px',
-		ease: Strong.EaseIn,
-		onComplete: function () {
-			$window.trigger('extra.hideResponsiveMenuComplete');
-		}
-	});
-	TweenMax.set($toMove, {
-		x: 0,
-		ease: Strong.EaseIn
-	});
-
-	// TIMELINE
-	timeline.to(opt.$menu, 0.4, {
-		x: 0,
-		ease: Quad.EaseOut
-	});
-	timeline.to($toMove, 0.5, {
-		x: opt.$menu.width() + 'px',
-		ease: Quad.EaseOut,
-		onComplete: function () {
-			$window.trigger('extra.showResponsiveMenuComplete');
-		}
-	}, '-=0.3');
-
-	// END OF TIMELINE
-	if (opt.appendTimeline) {
-		timeline = opt.appendTimeline(timeline);
-	}
-	/**************************
-	 *
-	 *
-	 * SHOW
-	 *
-	 *
-	 *************************/
-	function showMenu(fast) {
-		if (small || opt.everySizes) {
-			opt.$menu.css('visibility', 'visible');
-			menuOpen = true;
-			$html.addClass('menu-open');
-			$window.trigger('extra.showResponsiveMenu');
-			if (fast) {
-				timeline.totalProgress(1);
-			} else {
-				timeline.play();
+			// update the menu
+			function update() {
+				if (!smallBreakpoint && !opt.everySizes) {
+					$html.removeClass('responsive-menu').addClass('no-responsive-menu');
+				} else {
+					//$html.removeClass('no-responsive-menu').addClass('responsive-menu');
+				}
 			}
-		} else {
-			hideMenu(true);
-		}
-	}
 
-	/**************************
-	 *
-	 *
-	 * HIDE
-	 *
-	 *
-	 *************************/
-	function hideMenu(fast) {
-		menuOpen = false;
-		$html.removeClass('menu-open');
-		if (!small && !opt.everySizes) {
-			opt.$menu.removeAttr("style");
-			opt.$site.removeAttr("style");
-			opt.$menu.hide();
-		}
-		$window.trigger('extra.hideResponsiveMenu');
-		if (fast) {
-			timeline.totalProgress(0);
-		} else {
-			timeline.reverse();
-		}
-	}
+			// Update the menu animation timeline
+			function updateTimeline() {
 
-	$(document).on('click', function (e) {
-		var $target = $(e.target);
-		if (menuOpen && $target != opt.$menu && !$target.closest(opt.$menu).length && $target != opt.$button && !$target.closest(opt.$button).length && $target != opt.$button && !$target.closest($wpadminbar).length) {
-			hideMenu();
-		}
-	});
+				// Delete current timelinemax
+				if (timeline) {
+					TweenMax.set([$menu, opt.$site], {
+						clearProps: "all"
+					});
+					timeline.kill();
+				}
 
-	$(document).on('extra.responsive-resize', function(){
-		update();
-	});
+				// Recreate a new timelinemax
+				timeline = new TimelineMax({
+					paused: true,
+					onStart: function () {
+						$window.trigger('extra:menu:ShowStart');
+					},
+					onComplete: function () {
+						$window.trigger('extra:menu:ShowComplete');
+					},
+					onReverseComplete: function () {
+						$window.trigger('extra:menu:HideComplete');
+					}
+				});
 
-	function update() {
-		if (small) {
-			$toMove = opt.moveButton ? opt.$button : opt.$site;
-		} else {
-			$toMove = opt.moveButton ? [opt.$site, opt.$button] : opt.$site;
-		}
-		if (!small && !opt.everySizes) {
-			opt.$menu.removeAttr("style");
-			opt.$site.removeAttr("style");
-			opt.$menu.hide();
-			$html.removeClass('responsive-menu').addClass('no-responsive-menu');
-		} else {
-			$html.removeClass('no-responsive-menu').addClass('responsive-menu');
-		}
-	}
+				if(!smallBreakpoint && !opt.everySizes) {
+					return;
+				}
 
-	$window.on('extra.triggerHideResponsiveMenu', function () {
-		hideMenu();
-	}).on('extra.triggerShowResponsiveMenu', function () {
-		showMenu();
-	});
-}
+				// Prepended timeline
+				if (opt.prependTimeline) {
+					timeline = opt.prependTimeline(timeline);
+				}
+
+				// setter
+				TweenMax.set($menu, {
+					x: -$menu.outerWidth() + 'px',
+					ease: Strong.EaseIn
+				});
+				if ($toMove) {
+					TweenMax.set($toMove, {
+						x: 0,
+						ease: Strong.EaseIn
+					});
+				}
+
+				// TIMELINE
+				timeline.to($menu, 0.4, {
+					x: 0,
+					ease: Quad.EaseOut
+				});
+				if ($toMove) {
+					timeline.to($toMove, 0.5, {
+						x: $menu.outerWidth() + 'px',
+						ease: Quad.EaseOut,
+						onComplete: function () {
+							update();
+						}
+					}, '-=0.3');
+				} else {
+					timeline.addCallback(function () {
+						update();
+					});
+				}
+
+				// Appended timeline
+				if (opt.appendTimeline) {
+					timeline = opt.appendTimeline(timeline);
+				}
+			}
+
+			// breakpoint condition tester
+			function defaultSmallBreakpointTester() {
+				return $window.width() < opt.breakPointWidth;
+			}
+
+			/*********************************** TRIGGER EVENTS ***********************************/
+			$window.
+				// @fast: false = hide with animation, true = hide instantaneously
+				on('extra:menu:hide', function (event, fast) {
+					hideMenu(fast);
+				})
+				// @fast: false = hide with animation, true = hide instantaneously
+				.on('extra:menu:show', function (event, fast) {
+					showMenu(fast);
+				});
+
+			$window.on(opt.resizeEvent, function () {
+				smallBreakpoint = opt.smallBreakpointCondition();
+				updateTimeline();
+				menuOpen ? showMenu(true) : hideMenu(true);
+			});
+
+			opt.$button.on("click", function () {
+				menuOpen ? hideMenu() : showMenu();
+			});
+
+
+			/*********************************** DEFAULT BREAK POINT CHECKER VALUE ***********************************/
+			if (opt.smallBreakpointCondition === null) {
+				opt.smallBreakpointCondition = defaultSmallBreakpointTester;
+			}
+
+			smallBreakpoint = opt.smallBreakpointCondition();
+
+
+			/*********************************** INITIALIZE ***********************************/
+			updateTimeline();
+			opt.autoHide ? hideMenu(true) : showMenu(true);
+
+		});
+
+		return this;
+
+	};
+}(jQuery));
